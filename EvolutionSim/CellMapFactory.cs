002140ce4900a536979c735b1aa34edeadddf742
LDC7 <lihwar1@gmail.com>
2019-02-14 20:25:33 +0400

    Add Rules

--- EvolutionSim/CellMapFactory.cs
+++ EvolutionSim/CellMapFactory.cs
@@ -1,47 +1,49 @@
 ﻿namespace EvolutionSim
 {
-  internal abstract class CellMapFactory
-  {
-    internal static Cell GetCell(Cell.CellType type)
+    internal abstract class CellMapFactory
     {
-      switch (type)
-      {
-        case Cell.CellType.Empty:
-          return new Cell();
-        case Cell.CellType.Wall:
-          return new Cell
-          {
-            Value = new Wall(),
-            Type = Cell.CellType.Wall
-          };
-        case Cell.CellType.Food:
-          return new Cell
-          {
-            Value = new Food(),
-            Type = Cell.CellType.Food
-          };
-        case Cell.CellType.Poison:
-          return new Cell
-          {
-            Value = new Poison(),
-            Type = Cell.CellType.Poison
-          };
+        public static Rules Rules { get; set; }
 
-        default:
-          return new Cell();
-      }
-    }
+        internal static Cell GetCell(Cell.CellType type)
+        {
+            switch (type)
+            {
+                case Cell.CellType.Empty:
+                    return new Cell();
+                case Cell.CellType.Wall:
+                    return new Cell
+                    {
+                        Value = new Wall(),
+                        Type = Cell.CellType.Wall
+                    };
+                case Cell.CellType.Food:
+                    return new Cell
+                    {
+                        Value = Rules.RuleFood(),
+                        Type = Cell.CellType.Food
+                    };
+                case Cell.CellType.Poison:
+                    return new Cell
+                    {
+                        Value = Rules.RulePoison(),
+                        Type = Cell.CellType.Poison
+                    };
 
-    internal static void ToFood(Cell cell, Food food)
-    {
-      cell.Type = Cell.CellType.Food;
-      cell.Value = (ICell)food.Clone();
-    }
+                default:
+                    return new Cell();
+            }
+        }
 
-    internal static void ToPoison(Cell cell, Poison poison)
-    {
-      cell.Type = Cell.CellType.Poison;
-      cell.Value = (ICell)poison.Clone();
+        internal static void ToFood(Cell cell, Food food)
+        {
+            cell.Type = Cell.CellType.Food;
+            cell.Value = (ICell)food.Clone();
+        }
+
+        internal static void ToPoison(Cell cell, Poison poison)
+        {
+            cell.Type = Cell.CellType.Poison;
+            cell.Value = (ICell)poison.Clone();
+        }
     }
-  }
 }

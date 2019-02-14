b5715a6746dae4809d137f0dcd793a622108ea9d
LDC7 <lihwar1@gmail.com>
2019-02-14 20:17:31 +0400

    DNA done Gen done

--- EvolutionSim/DNA.cs
+++ EvolutionSim/DNA.cs
@@ -1,15 +1,46 @@
 ﻿namespace EvolutionSim
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class DNA
     {
+        private static Random random = new Random();
         internal Gen[] gens;
-        internal int GensCount { get; } = 100;
+        internal static int GensCount { get; } = 100;
 
+        internal static DNA CreateRandom()
+        {
+            DNA res = new DNA();
+            Gen[] dnaGens = new Gen[GensCount];
+            for (int i = 0; i < GensCount; i++)
+            {
+                dnaGens[i] = new Gen((Gen.GenType)random.Next(Gen.MinValue, Gen.MaxValue + 1));
+            }
+
+            res.gens = dnaGens;
+            return res;
+        }
+
+        internal void Mutate(double chance)
+        {
+            for (int i = 0; i < GensCount; i++)
+            {
+                if (random.NextDouble() >= chance)
+                {
+                    gens[i] = new Gen((Gen.GenType)random.Next(Gen.MinValue, Gen.MaxValue + 1));
+                }
+            }
+        }
+
+        internal static DNA Mix(DNA dna1, DNA dna2)
+        {
+            DNA newDNA = new DNA();
+            for (int i = 0; i < GensCount; i++)
+            {
+                newDNA.gens[i] = random.Next(2) > 0 ? dna1.gens[i] : dna2.gens[i];
+            }
+
+            return newDNA;
+        }
     }
 }
--- EvolutionSim/Gen.cs
+++ EvolutionSim/Gen.cs
@@ -4,18 +4,54 @@
 
     internal class Gen
     {
-        private int value;
-        internal int MinValue { get; } = 0;
-        internal int MaxValue { get; } = 0;
+        internal static int MinValue { get; } = 0;
+        internal static int MaxValue { get; } = 17;
+        private GenType value;
 
-        internal int Value
+        internal GenType Value
         {
             get { return this.value; }
             set
             {
-                int tempVar = value < MinValue ? Math.Abs(value) : value;
-                this.value = tempVar > MaxValue ? tempVar % MaxValue : tempVar;
+                int tempVar = (int)value < MinValue ? Math.Abs((int)value) : (int)value;
+                this.value = (GenType)(tempVar > MaxValue ? tempVar % MaxValue : tempVar);
             }
+        }
+
+        internal Gen(GenType gen)
+        {
+            Value = gen;
+        }
+
+        internal enum GenType
+        {
+            // Move
+            MoveNW = 0,
+            MoveNN = 1,
+            MoveNE = 2,
+            MoveEE = 3,
+            MoveSE = 4,
+            MoveSS = 5,
+            MoveSW = 6,
+            MoveWW = 7,
+
+            // Rotation
+            Rotation1 = 8,
+            Rotation2 = 9,
+            Rotation3 = 10,
+            Rotation4 = 11,
+            Rotation5 = 12,
+            Rotation6 = 13,
+            Rotation7 = 14,
+
+            // Look
+            Look = 15,
+
+            // Pick
+            Pick = 16,
+
+            // Attack
+            Attack = 17,
         }
     }
 }
